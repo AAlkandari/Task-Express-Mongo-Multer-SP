2798d1e81221ee9d64038828b96ea833e402f38e
AAlkandari <96235631+AAlkandari@users.noreply.github.com>
2022-02-13 07:55:07 +0300

    adding express.static

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   );
   next();
 });
-
+app.use("/media", express.static(path.join(__dirname, "media")));
 // Routes
 app.use("/products", productsRoutes);
 app.use("/shops", shopsRoutes);
